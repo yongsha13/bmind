8d1a674744eb6830f72b4d798ce55130558cc9de
setin <9600662@qq.com>
2016-01-27 15:48:50 +0800

    移动端下载页面修改UI 16.01.27-a

---
+++ home/js/page-m.js
@@ -0,0 +1,37 @@
+/**
+ * Created by setin on 2016/1/12.
+ */
+$(function(){
+    slidePic.init();
+    $(window).resize(function(){
+        slidePic.getWidth();
+    });
+});
+
+var slidePic = {
+    picContainer:null,
+    posContainer:null,
+    width:0,
+    stepTime:5000,
+    pause:false,
+    init:function(){
+        this.picContainer = $('.slide-box');
+        this.posContainer = $('.slide-box').next('.pos');
+        this.getWidth();
+        this.roll();
+        this.picContainer
+            .on('mouseover',function(){slidePic.pause = true; })
+            .on('mouseout', function(){slidePic.pause = false;})
+    },
+    getWidth:function(){
+        this.width = this.picContainer.find('img').width();
+    },
+    roll:function(){
+        setInterval(function(){
+            !slidePic.pause && slidePic.picContainer.animate({textIndent:-slidePic.width},1000,function(){
+                slidePic.posContainer.find('span').toggleClass('cur');
+                slidePic.picContainer.css('text-indent',0).find('a').append(slidePic.picContainer.find('img:eq(0)'));
+            })
+        },this.stepTime);
+    }
+}
