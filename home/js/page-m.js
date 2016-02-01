5abc28294b561c758b940b05c923deaeb503bc44
setin <9600662@qq.com>
2016-02-01 10:23:51 +0800

    下载页面修改UI 16.02.01-a

--- home/js/page-m.js
+++ home/js/page-m.js
@@ -13,6 +13,7 @@ var slidePic = {
     posContainer:null,
     width:0,
     stepTime:5000,
+    cur:0,
     pause:false,
     init:function(){
         this.picContainer = $('.slide-box');
@@ -29,7 +30,8 @@ var slidePic = {
     roll:function(){
         setInterval(function(){
             !slidePic.pause && slidePic.picContainer.animate({textIndent:-slidePic.width},1000,function(){
-                slidePic.posContainer.find('span').toggleClass('cur');
+                slidePic.cur++;
+                slidePic.posContainer.find('span:eq('+(slidePic.cur%4)+')').addClass('cur').siblings().removeClass('cur');
                 slidePic.picContainer.css('text-indent',0).find('a').append(slidePic.picContainer.find('img:eq(0)'));
             })
         },this.stepTime);
